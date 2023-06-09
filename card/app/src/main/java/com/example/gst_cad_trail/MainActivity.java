bd15eb6fffe9f4fe97250cd85dfb8808bfcbb339
Anushree M <85032173+anushree1125@users.noreply.github.com>
2023-06-09 12:24:00 +0530

    Add files via upload

---
+++ card/app/src/main/java/com/example/gst_cad_trail/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.gst_cad_trail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.card.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
